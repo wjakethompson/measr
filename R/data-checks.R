3c02fd416056c0041becc24993a719b12d71eded
Jake Thompson <wjakethompson@gmail.com>
2022-04-26 08:21:22 -0500

    enforce qmatrix restrictions (same items as data)

--- R/data-checks.R
+++ R/data-checks.R
@@ -26,22 +26,32 @@ check_data <- function(x, name, identifier, missing) {
   }
   x <- dplyr::mutate(x, dplyr::across(!dplyr::all_of(!!identifier), as.integer))
 
+
+  item_names <- colnames(x)
+  if (!is.null(identifier)) {
+    item_names <- item_names[-which(item_names == rlang::as_name(identifier))]
+  }
+
   # move to long format for Stan
   if (is.null(identifier)) {
     x <- x %>%
       tibble::rowid_to_column(var = "resp_id") %>%
-      tidyr::pivot_longer(cols = -.data$resp_id, names_to = "item",
+      tidyr::pivot_longer(cols = -.data$resp_id, names_to = "item_id",
                           values_to = "score") %>%
       dplyr::filter(!(.data$score %in% missing)) %>%
-      dplyr::mutate(score = as.integer(.data$score))
-    identifier <- "resp_id"
-    identifier <- enquo(identifier)
+      dplyr::mutate(score = as.integer(.data$score),
+                    resp_id = factor(.data$resp_id),
+                    item_id = factor(.data$item_id, levels = item_names))
   } else {
+    resp_names <- dplyr::pull(x, !!identifier)
     x <- x %>%
-      tidyr::pivot_longer(cols = -!!identifier, names_to = "item",
+      tidyr::pivot_longer(cols = -!!identifier, names_to = "item_id",
                           values_to = "score") %>%
       dplyr::filter(!(.data$score %in% missing)) %>%
-      dplyr::mutate(score = as.integer(.data$score))
+      dplyr::rename(resp_id = !!identifier) %>%
+      dplyr::mutate(score = as.integer(.data$score),
+                    resp_id = factor(.data$resp_id, levels = resp_names),
+                    item_id = factor(.data$item_id, levels = item_names))
   }
 
   if (!all(x$score %in% c(0L, 1L))) {
@@ -49,21 +59,24 @@ check_data <- function(x, name, identifier, missing) {
                        must = "contain only 0 or 1 for non-missing scores")
   }
 
+  # x <- x %>%
+  #   dplyr::left_join(
+  #     tibble::rowid_to_column(x %>%
+  #                               dplyr::select(dplyr::all_of(!!identifier)) %>%
+  #                               dplyr::distinct(),
+  #                             var = "stan_resp_id"),
+  #     by = rlang::as_name(identifier)) %>%
+  #   dplyr::left_join(
+  #     tibble::rowid_to_column(dplyr::distinct(x, .data$item_id),
+  #                             var = "stan_item_id"),
+  #     by = "item_id") %>%
+  #   dplyr::select(dplyr::all_of(!!identifier), .data$stan_resp_id,
+  #                 .data$item_id, .data$stan_item_id,
+  #                 .data$score) %>%
+  #   dplyr::arrange(.data$stan_resp_id, .data$stan_item_id)
+
   x <- x %>%
-    dplyr::left_join(
-      tibble::rowid_to_column(x %>%
-                                dplyr::select(dplyr::all_of(!!identifier)) %>%
-                                dplyr::distinct(),
-                              var = "stan_resp_id"),
-      by = rlang::as_name(identifier)) %>%
-    dplyr::left_join(
-      tibble::rowid_to_column(dplyr::distinct(x, .data$item),
-                              var = "stan_item_id"),
-      by = "item") %>%
-    dplyr::select(dplyr::all_of(!!identifier), .data$stan_resp_id,
-                  .data$item, .data$stan_item_id,
-                  .data$score) %>%
-    dplyr::arrange(.data$stan_resp_id, .data$stan_item_id)
+    dplyr::arrange(.data$resp_id, .data$item_id)
 
   if (!tibble::is_tibble(x)) {
     tibble::as_tibble(x)
@@ -72,18 +85,52 @@ check_data <- function(x, name, identifier, missing) {
   }
 }
 
-check_qmatrix <- function(x, name) {
+check_qmatrix <- function(x, identifier, item_levels, name) {
+  if (!is.null(identifier)) identifier <- enquo(identifier)
+
   if (!("data.frame" %in% class(x))) {
     abort_bad_argument(name, must = "be a data frame")
   }
 
-  if (!all(sapply(x, is.numeric))) {
+  if (nrow(x) != length(item_levels)) {
+    abort_bad_argument(name, must = glue::glue("have the same number of rows ",
+                                               "as columns of items in `data`"))
+  }
+
+  #check that item ids match item levels
+  if (is.null(identifier)) {
+    x <- x %>%
+      dplyr::mutate(item_id = item_levels,
+                    item_id = factor(.data$item_id, levels = item_levels),
+                    .before = 1)
+  } else {
+    item_names <- dplyr::pull(x, !!identifier)
+    if (!all(item_levels %in% item_names)) {
+      missing_items <- setdiff(item_levels, item_names)
+      abort_bad_argument(name,
+                         must = glue::glue("include all items in `data`.
+                                           Missing items: {missing_items}"))
+    }
+    if (!all(item_names %in% item_levels)) {
+      extra_items <- setdiff(item_names, item_levels)
+      abort_bad_argument(name,
+                         must = glue::glue("only include items found in `data`.
+                                           Extra items: {extra_items}"))
+    }
+    x <- x %>%
+      dplyr::rename(item_id = !!identifier) %>%
+      dplyr::mutate(item_id = factor(.data$item_id, levels = item_levels)) %>%
+      dplyr::arrange(.data$item_id)
+  }
+
+  if (!all(sapply(dplyr::select(x, -.data$item_id), is.numeric))) {
     abort_bad_argument(name, must = "contain only numeric columns")
   }
-  x <- dplyr::mutate(x, dplyr::across(dplyr::everything(), as.integer))
+  x <- dplyr::mutate(x, dplyr::across(!dplyr::all_of(!!identifier), as.integer))
 
-  if (!all(sapply(x, \(.x) all(.x %in% c(0L, 1L))))) {
-    abort_bad_argument(name, must = "contain only 0 or 1")
+  if (!all(sapply(dplyr::select(x, -.data$item_id),
+                  function(.x) all(.x %in% c(0L, 1L))))) {
+    abort_bad_argument(name, must = "contain only 0 or 1 in attribute columns")
   }
 
   if (!tibble::is_tibble(x)) {
