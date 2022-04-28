285c3afc3d27af0cd5ff5a7990d13b814f5412dc
Jake Thompson <wjakethompson@gmail.com>
2022-04-28 09:39:34 -0500

    document measrfit class

--- R/measrfit-class.R
+++ R/measrfit-class.R
@@ -1,23 +1,33 @@
 #' Class `measrfit` of models fitted with the **measr** package
 #'
-#' Something...
+#' Models fitted with the **measr** package are represented as a `measrfit`
+#' object, which contains the posterior draws, Stan code, priors, and other
+#' relevant information.
 #'
 #' @name measrfit-class
 #' @aliases measrfit
 #' @docType class
 #'
-#' @slot data
+#' @slot data The data and Q-matrix used to estimate the model.
 #' @slot prior A [measrprior][measrprior()] object containing information on the
 #'   priors used in the model.
 #' @slot stancode The model code in **Stan** language.
-#' @slot algorithm
-#' @slot backend
-#' @slot model
-#' @slot fit
-#' @slot criteria
-#' @slot reliability
-#' @slot file
-#' @slot version
+#' @slot method The method used to fit the model.
+#' @slot algorithm The name of the algorithm used to fit the model.
+#' @slot backend The name of the backend used to fit the model.
+#' @slot model The fitted Stan model. This will object of class
+#'   [rstan::stanfit-class] unless `backend == "cmdstanr"` and
+#'   `return_stanfit = FALSE` was specified when fitting the model.
+#' @slot model_fit An empty list for adding model fit information after
+#'   fitting the model.
+#' @slot criteria An empty list for adding information criteria after fitting
+#'   the model.
+#' @slot reliability An empty list for adding reliability information after
+#'   fitting the model.
+#' @slot file Optional name of a file which the model objects was saved to
+#'   or loaded from.
+#' @slot version The versions of **measr**, **rstan**, **cmdstanr**, and/or
+#'   **Stan** that were used to fit the model.
 #'
 #' @seealso [measr_dcm()]
 NULL
