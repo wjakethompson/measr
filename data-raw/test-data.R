30af65cf337d63ab87112bcd73d29cbb622836e1
Jake Thompson <wjakethompson@gmail.com>
2023-01-11 13:36:51 -0600

    add mplus parameters for testing lcdm estimates

--- data-raw/test-data.R
+++ data-raw/test-data.R
@@ -59,8 +59,24 @@ true_dinoa <- dinao_params %>%
               mutate(param = paste0("nu[", parm1, "]")) %>%
               select(param, true = prob))
 
-## lcdm
+## lcdm - estimated ecpe in Mplus
+library(fitsim)
+library(MplusAutomation)
 
+lcdm <- readModels(here("data-raw", "mplus", "lcdm.out"))
+
+class_params <- lcdm$class_counts$modelEstimated %>%
+  as_tibble() %>%
+  select(parameter = class, true = proportion) %>%
+  mutate(parameter = paste0("nu[", parameter, "]"))
+
+item_params <- lcdm$parameters$unstandardized %>%
+  as_tibble() %>%
+  filter(paramHeader == "New.Additional.Parameters") %>%
+  select(parameter = param, true = est) %>%
+  mutate(parameter = str_to_lower(parameter))
+
+true_lcdm <- bind_rows(item_params, class_params)
 
 
 # generate data ----------------------------------------------------------------
@@ -146,5 +162,5 @@ ggplot(param_compare, aes(x = true, y = mean_dino)) +
 
 
 # save data --------------------------------------------------------------------
-use_data(q_matrix, true_dinoa, true_profiles, dina_data, dino_data,
+use_data(q_matrix, true_dinoa, true_profiles, dina_data, dino_data, true_lcdm,
          internal = TRUE, overwrite = TRUE)
