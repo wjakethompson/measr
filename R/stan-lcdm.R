faa539800910e82ecd2e7072d2bbe50fe1508bf2
Jake Thompson <wjakethompson@gmail.com>
2023-01-20 16:08:05 -0600

    add generated quantities to lcdm

--- R/stan-lcdm.R
+++ R/stan-lcdm.R
@@ -1,4 +1,4 @@
-lcdm_script <- function(qmatrix, prior = NULL) {
+lcdm_script <- function(qmatrix, prior = NULL, gqs = FALSE) {
   # data block -----
   data_block <- glue::glue(
     "data {{
@@ -189,12 +189,44 @@ lcdm_script <- function(qmatrix, prior = NULL) {
     "}}", .sep = "\n"
   )
 
+  # generated quantities block -----
+  gqs_block <- glue::glue(
+    "generated quantities {{",
+    "  matrix[R,C] prob_resp_class;   // post prob of respondent R in class C",
+    "  matrix[R,A] prob_resp_attr;    // post prob of respondent R master A",
+    "",
+    "  for (r in 1:R) {{",
+    "    row_vector[C] prob_joint;",
+    "    for (c in 1:C) {{",
+    "      real log_items[num[r]];",
+    "      for (m in 1:num[r]) {{",
+    "        int i = ii[start[r] + m - 1];",
+    "        log_items[m] = y[start[r] + m - 1] * log(pi[i,c]) +",
+    "                       (1 - y[start[r] + m - 1]) * log(1 - pi[i,c]);",
+    "      }}",
+    "      prob_joint[c] = Vc[c] * exp(sum(log_items));",
+    "    }}",
+    "    prob_resp_class[r] = prob_joint / sum(prob_joint);",
+    "  }}",
+    "",
+    "  for (r in 1:R) {{",
+    "    for (a in 1:A) {{",
+    "      row_vector[C] prob_attr_class;",
+    "      for (c in 1:C) {{",
+    "        prob_attr_class[c] = prob_resp_class[r,c] * Alpha[c,a];",
+    "      }}",
+    "      prob_resp_attr[r,a] = sum(prob_attr_class);",
+    "    }}",
+    "  }}",
+    "}}", .sep = "\n"
+  )
+
   # combine blocks -----
   full_script <- glue::glue(
     "{data_block}",
     "{parameters_block}",
     "{transformed_parameters_block}",
-    "{model_block}",
+    "{ifelse(gqs, gqs_block, model_block)}",
     .sep = "\n"
   )
 
