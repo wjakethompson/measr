cbdc676652dd33eaaa9138baf9cd19a14f74a847
Jake Thompson <wjakethompson@gmail.com>
2023-01-30 10:45:22 -0600

    add tests for `check_double()`

--- tests/testthat/test-data-checks.R
+++ tests/testthat/test-data-checks.R
@@ -285,6 +285,51 @@ test_that("check_integer", {
   expect_equal(check_integer(6, lb = 0, name = "check1"), 6L)
 })
 
+test_that("check_double", {
+  err <- rlang::catch_cnd(check_double("a", name = "check1"))
+  expect_s3_class(err, "error_bad_argument")
+  expect_equal(err$arg, "check1")
+  expect_match(err$message, "numeric scalar")
+  expect_equal(err$not, "character")
+
+  err <- rlang::catch_cnd(check_double(list(c(1, 2), 3), name = "check1"))
+  expect_s3_class(err, "error_bad_argument")
+  expect_equal(err$arg, "check1")
+  expect_match(err$message, "length 1")
+  expect_equal(err$not, 2L)
+
+  err <- rlang::catch_cnd(check_double(NA_real_, name = "check1"))
+  expect_s3_class(err, "error_bad_argument")
+  expect_equal(err$arg, "check1")
+  expect_match(err$message, "non-missing")
+
+  err <- rlang::catch_cnd(check_double(-1, lb = 0L, name = "check1"))
+  expect_s3_class(err, "error_bad_argument")
+  expect_equal(err$arg, "check1")
+  expect_match(err$message, "greater than 0")
+
+  err <- rlang::catch_cnd(check_double(1, ub = 0L, name = "check1"))
+  expect_s3_class(err, "error_bad_argument")
+  expect_equal(err$arg, "check1")
+  expect_match(err$message, "less than 0")
+
+  err <- rlang::catch_cnd(check_double(4L, lb = 0L, ub = 3L, name = "check1"))
+  expect_s3_class(err, "error_bad_argument")
+  expect_equal(err$arg, "check1")
+  expect_match(err$message, "between 0 and 3")
+
+  err <- rlang::catch_cnd(check_double(0, lb = 0, inclusive = FALSE,
+                                        name = "check1"))
+  expect_s3_class(err, "error_bad_argument")
+  expect_equal(err$arg, "check1")
+  expect_match(err$message, "greater than 0")
+
+  expect_equal(check_double(0.98, name = "check1"), 0.98)
+  expect_equal(check_double(0.1, name = "check1"), 0.1)
+  expect_equal(check_double(0, lb = 0, inclusive = TRUE, name = "check1"), 0L)
+  expect_equal(check_double(0.975, lb = 0, ub = 1, name = "check1"), 0.975)
+})
+
 test_that("check_character", {
   err <- rlang::catch_cnd(check_character(1L, name = "check1"))
   expect_s3_class(err, "error_bad_argument")
