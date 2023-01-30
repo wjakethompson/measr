fa4ad4e141bf772da2e55067123784d5d9701f23
Jake Thompson <wjakethompson@gmail.com>
2023-01-30 13:30:50 -0600

    add prediction tests

---
+++ tests/testthat/test-methods.R
@@ -0,0 +1,44 @@
+test_that("returned predictions have correct dimensions and names", {
+  num_att <- ncol(rstn_mdm_lcdm$data$qmatrix) - 1
+
+  mod_preds <- predict(rstn_mdm_lcdm, summary = FALSE)
+  expect_equal(names(mod_preds), c("class_probabilities",
+                                   "attribute_probabilities"))
+  expect_equal(colnames(mod_preds$class_probabilities),
+               c(".chain", ".iteration", ".draw", "respondent", "[0]", "[1]"))
+  expect_equal(colnames(mod_preds$attribute_probabilities),
+               c(".chain", ".iteration", ".draw", "respondent",
+                 "multiplication"))
+  expect_equal(nrow(mod_preds$class_probabilities), nrow(mdm_data))
+  expect_equal(nrow(mod_preds$attribute_probabilities), nrow(mdm_data))
+
+  mod_preds <- predict(rstn_mdm_lcdm, summary = TRUE)
+  expect_equal(names(mod_preds), c("class_probabilities",
+                                   "attribute_probabilities"))
+  expect_equal(colnames(mod_preds$class_probabilities),
+               c("respondent", "class", "mean", "2.5%", "97.5%"))
+  expect_equal(colnames(mod_preds$attribute_probabilities),
+               c("respondent", "attribute", "mean", "2.5%", "97.5%"))
+  expect_equal(nrow(mod_preds$class_probabilities),
+               nrow(mdm_data) * (2 ^ num_att))
+  expect_equal(nrow(mod_preds$attribute_probabilities),
+               nrow(mdm_data) * num_att)
+})
+
+test_that("mdm probabilities are accurate", {
+  mdm_preds <- predict(rstn_mdm_lcdm, summary = TRUE)
+
+  measr_class <- mdm_preds$class_probabilities %>%
+    dplyr::select("respondent", "class", "mean") %>%
+    tidyr::pivot_wider(names_from = "class", values_from = "mean") %>%
+    dplyr::select(-"respondent") %>%
+    as.matrix() %>%
+    unname()
+  expect_equal(measr_class, mdm_lldcm$posterior, tolerance = 0.01)
+
+  measr_attr <- mdm_preds$attribute_probabilities %>%
+    dplyr::select("mean") %>%
+    as.matrix() %>%
+    unname()
+  expect_equal(measr_attr, mdm_lldcm$eap, tolerance = 0.01)
+})
