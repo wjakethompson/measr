0220be46560a40e2dfcc06ed9daa0fc0ff9b158e
Jake Thompson <wjakethompson@gmail.com>
2023-01-30 10:39:14 -0600

    add reliability functionality

--- R/reliability.R
+++ R/reliability.R
@@ -1,14 +1,185 @@
-reliability <- function(x) {
+#' @export
+reliability <- function(model) {
   UseMethod("reliability")
 }
 
-reliability.measrdcm <- function(x) {
+#' Estimate the reliability of a diagnostic classification model
+#'
+#' Estimate the reliability of an estimated diagnostic classification model at
+#' the pattern or attribute level. Pattern-level reliability represents the
+#' classification consistency and accuracy of placing students into an overall
+#' mastery profile. Rather than an overall profile, attributes can also be
+#' scored individually. In this case, classification consistency and accuracy
+#' should be evaluated for each individual attribute, rather than the overall
+#' profile. This is referred to as the *modal a posteriori* (MAP) reliability.
+#' Finally, it may be desirable to report results as the probability of
+#' proficiency or mastery on each attribute instead of a proficient/not
+#' proficient classification. In this case, the reliability of the posterior
+#' probability should be reported. THis is the *expected a poteriori* (EAP)
+#' reliability.
+#'
+#' @param model A DCM estimated by [measr_dcm()].
+#'
+#' @details The pattern-level reliability (`pattern_reliability`) statistics are
+#' described in Cui et al. (2012). Attribute-level classification reliability
+#' statistics (`map_reliability`) are described in Johnson & Sinharay (2018).
+#' Reliability statistics for the posterior mean of the skill indicators (i.e.,
+#' the mastery or proficiency probabilities; `eap_reliability`) are described in
+#' Johnson & Sinharay (2019).
+#'
+#' @returns A list with 3 elements:
+#'   * `pattern_reliability`: The pattern-level accuracy (`p_a`) and consistency
+#'     (`p_c`) described by Cui et al. (2012).
+#'   * `map_reliability`: A list with 2 elements: `accuracy` and `consistency`,
+#'     which include the attribute-level classification reliability statistics
+#'     described by Johnson & Sinharay (2018).
+#'   * `eap_reliability`: The attribute-level posterior probability reliability
+#'     statistics described by Johnson & Sinharay (2020).
+#'
+#' @references Cui, Y., Gierl, M. J., & Chang, H.-H. (2012). Estimating
+#'   classification consistency and accuracy for cognitive diagnostic
+#'   assessment. *Journal of Educational Measurement, 49*(1), 19-38.
+#'   \url{https://doi.org/10.1111/j.1745-3984.2011.00158.x}
+#' @references Johnson, M. S., & Sinharay, S. (2018). Measures of agreement to
+#'   assess attribute-level classification accuracy and consistency for
+#'   cognitive diagnostic assessments. *Journal of Educational Measurement,
+#'   55*(4), 635-664. \url{https://doi.org/10.1111/jedm.12196}
+#' @references Johnson, M. S., & Sinharay, S. (2020). The reliability of the
+#'   posterior probability of skill attainment in diagnostic classification
+#'   models. *Journal of Educational and Behavioral Statistics, 45*(1), 5-31.
+#'   \url{https://doi.org/10.3102/1076998619864550}
+#'
+#' @export
+reliability.measrdcm <- function(model) {
   # coerce model into a list of values required for reliability
-  obj <- if (x$method == "mcmc") {
-    create_reli_list_mcmc(x)
-  } else if (x$method == "optim") {
-    create_reli_list_optim(x)
+  obj <- reli_list(model)
+  att_names <- model$data$qmatrix %>%
+    dplyr::select(-"item_id") %>%
+    colnames()
+
+  tbl <- obj$acc
+  p <- obj$prev
+  k <- length(p)
+  acc <- apply(tbl, 3, function(m) {
+    sum(diag(m)) / sum(m)
+  })
+  tp_a <- apply(tbl, 3, function(m) {
+    m[2, 2] / (sum(m[, 2]))
+  })
+  tn_a <- apply(tbl, 3, function(m) {
+    m[1, 1] / sum(m[, 1])
+  })
+  youden_a <- tp_a + tn_a - 1
+  mx <- apply(tbl, 3, function(m) {
+    max(apply(m / sum(m), 2, sum))
+  })
+  lambda_a <- (acc - mx) / (1 - mx)
+  tetra_a <- apply(tbl, 3, function(m) {
+    psych::tetrachoric(m)$rho
+  })
+  kap_base <- p * p + (1 - p) * (1 - p)
+  kappa_a <- (acc - kap_base) / kap_base
+
+  tbl <- obj$consist
+
+  consist <- apply(tbl, 3, function(m) {
+    m[m < 1e-10] <- 0
+    sum(diag(m)) / sum(m)
+  })
+  tp_c <- apply(tbl, 3, function(m) {
+    m[m < 1e-10] <- 0
+    m[2, 2] / (sum(m[, 2]))
+  })
+  tn_c <- apply(tbl, 3, function(m) {
+    m[m < 1e-10] <- 0
+    m[1, 1] / sum(m[, 1])
+  })
+  youden_c <- tp_c + tn_c - 1
+  mx <- apply(tbl, 3, function(m) {
+    m[m < 1e-10] <- 0
+    max(apply(m / sum(m), 2, sum))
+  })
+  lambda_c <- (consist - mx) / (1 - mx)
+
+  tetra_c <- apply(tbl, 3, function(m) {
+    m[m < 1e-10] <- 0
+    psych::tetrachoric(m)$rho
+  })
+  p <- apply(tbl, 3, function(m) {
+    sum(m[2, ])
+  })
+  kap_base <- p * p + (1 - p) * (1 - p)
+  kappa_c <- (acc - kap_base) / kap_base
+
+  all_a <- as.matrix(create_profiles(k))
+  p_prime <- apply(sweep(apply(obj$posterior, 2, function(v) {
+    out <- tapply(v, apply(obj$posterior, 1, which.max), sum) / length(v)
+    if (dim(out) == 1) out <- as.array(c(out, `2` = 0))
+    return(out)
+  }),
+  2, obj$pxi, "/") ^ 2, 2, sum)
+  pc <- sum(p_prime * obj$pxi)
+  pc1 <- p_prime %*% (obj$pxi * all_a) / p
+  pc0 <- p_prime %*% (obj$pxi * (1 - all_a)) / (1 - p)
+  pc_prime <- as.vector(pc1 * pc1 * p + pc0 * pc0 * (1 - p))
+  pa <- mean(apply(obj$posterior, 1, max))
+
+  res_map_acc <- list(tibble::enframe(acc, name = "attribute", value = "acc"),
+                      tibble::enframe(lambda_a, name = "attribute",
+                                      value = "lambda_a"),
+                      tibble::enframe(kappa_a, name = "attribute", value = "kappa_a"),
+                      tibble::enframe(youden_a, name = "attribute",
+                                      value = "youden_a"),
+                      tibble::enframe(tetra_a, name = "attribute", value = "tetra_a"),
+                      tibble::enframe(tp_a, name = "attribute", value = "tp_a"),
+                      tibble::enframe(tn_a, name = "attribute", value = "tn_a")) %>%
+    purrr::reduce(dplyr::full_join, by = "attribute") %>%
+    tibble::remove_rownames()
+
+  gammak <- purrr::set_names(obj$gamma, att_names)
+  pc_prime <- purrr::set_names(pc_prime, att_names)
+  res_map_con <- list(tibble::enframe(consist, name = "attribute", value = "consist"),
+                      tibble::enframe(lambda_c, name = "attribute",
+                                      value = "lambda_c"),
+                      tibble::enframe(kappa_c, name = "attribute", value = "kappa_c"),
+                      tibble::enframe(youden_c, name = "attribute",
+                                      value = "youden_c"),
+                      tibble::enframe(tetra_c, name = "attribute", value = "tetra_c"),
+                      tibble::enframe(tp_c, name = "attribute", value = "tp_c"),
+                      tibble::enframe(tn_c, name = "attribute", value = "tn_c"),
+                      tibble::enframe(gammak, name = "attribute", value = "gammak"),
+                      tibble::enframe(pc_prime, name = "attribute",
+                                      value = "pc_prime")) %>%
+    purrr::reduce(dplyr::full_join, by = "attribute") %>%
+    tibble::remove_rownames()
+
+  ## Reliability for EAP  ##
+  tmp_fun <- function(v) {
+    m <- matrix(c(sum(v * v), sum(v * (1 - v)), sum(v * (1 - v)),
+                  sum((1 - v) * (1 - v))), 2, 2)
+    rho_tb <- psych::tetrachoric(m)$rho
+    rho_bs <- m[2, 2] / (m[2, 2] + m[2, 1]) - m[1, 2] / (m[1, 1] + m[1, 2])
+    ind_info <- ifelse(.5 - abs(v - .5) < 1e-12, 0, v * log(v) + (1 - v) *
+                         log(1 - v))
+    post_info <- mean(ind_info)
+    prior <- mean(v)
+    prior_info <- prior * log(prior) + (1 - prior) * log(1 - prior)
+    rho_i <- 1 - exp(-2 * (post_info - prior_info))
+    pf_num <- sum(apply(v * obj$posterior, 2, mean) ^ 2 / obj$pxi) - prior ^ 2
+    pf_den <- mean(v * v) - prior ^ 2
+    rho_pf <- pf_num / pf_den
+    c(rho_pf, rho_bs, rho_i, rho_tb)
   }
+  res_eap <- apply(obj$eap, 2, tmp_fun)
+  res_eap <- t(res_eap)
 
+  res_eap <- as.data.frame(res_eap) %>%
+    tibble::as_tibble() %>%
+    magrittr::set_colnames(c("rho_pf", "rho_bs", "rho_i", "rho_tb")) %>%
+    tibble::add_column(attribute = att_names, .before = 1)
 
+  list(pattern_reliability = c(p_a = pa, p_c = pc),
+       map_reliability = list(accuracy = res_map_acc,
+                              consistency = res_map_con),
+       eap_reliability = res_eap)
 }
--- R/utils-reliability.R
+++ R/utils-reliability.R
@@ -1,14 +1,106 @@
-create_reli_list_optim <- function(model) {
-  N <- nrow(model$data_lookup$resp_lookup)
-  K <- 1L
+clean_predicted_probs <- function(x, resp_id) {
+  x %>%
+    dplyr::select(-c(".chain", ".iteration", ".draw")) %>%
+    dplyr::summarize(dplyr::across(where(is.double),
+                                   \(x) mean(x, na.rm = TRUE)),
+                     .by = !!resp_id) %>%
+    dplyr::arrange(!!resp_id) %>%
+    dplyr::select(-!!resp_id)
+}
+
+create_reli_list <- function(N, K, M, A, pAhatA, attr_probs, class_probs,
+                             ahat, amean, pAx, pxi, att_names) {
+  ### Accuracy and Consistency -----
+  acc <- double(4 * K)
+  consist <- double(4 * K)
+  gamma <- double(K)
+  prev <- double(K)
+
+  for (i in (seq_len(M) - 1)) {
+    for (k in (seq_len(K) - 1)) {
+      if (A[(i + k * M) + 1] == 1) {
+        prev[k + 1] <- prev[k + 1] + pxi[i + 1]
+      }
+    }
+  }
+
+  for (i in (seq_len(N) - 1)) {
+    for (k in (seq_len(K) - 1)) {
+      acc[(k * 4 + ahat[(i + k * N) + 1]) + 1] <-
+        acc[(k * 4 + ahat[(i + k * N) + 1]) + 1] + (1 - amean[(i + k * N) + 1])
+      acc[(k * 4 + 2 + ahat[(i + k * N) + 1]) + 1] <-
+        acc[(k * 4 + 2 + ahat[(i + k * N) + 1]) + 1] + (amean[(i + k * N) + 1])
+      gamma[k + 1] <- gamma[k + 1] +
+        (amean[(i + k * N) + 1] * amean[(i + k * N) + 1] +
+           (1 - amean[(i + k * N) + 1]) * (1 - amean[(i + k * N) + 1]))
+
+      for (m in (seq_len(M) - 1)) {
+        if (ahat[(i + k * N) + 1] == 1) {
+          pAhatA[(m + k * M) + 1] <- pAhatA[(m + k * M) + 1] +
+            pAx[(m + i * M) + 1]
+        }
+      }
+    }
+  }
+  new_acc <- array(acc, c(2, 2, K),
+                   dimnames = list(c("ahat.0", "ahat.1"),
+                                   c("a.0", "a.1"),
+                                   att_names))
+
+  for (k in (seq_len(K) - 1)) {
+    gamma[k + 1] <- gamma[k + 1] / (N)
+  }
+  for (m in (seq_len(M) - 1)) {
+    for (k  in (seq_len(K) - 1)) {
+      pAhatA[(m + k * M) + 1] <- pAhatA[(m + k * M) + 1] /
+        ((N + 0.0) * pxi[m + 1])
+      consist[(4 * k) + 1] <- consist[(4 * k) + 1] +
+        ((1.0 - pAhatA[(m + k * M) + 1]) * (1.0 - pAhatA[(m + k * M) + 1]) *
+           pxi[m + 1])
+      consist[(4 * k + 1) + 1] <- consist[(4 * k + 1) + 1] +
+        ((1.0 - pAhatA[(m + k * M) + 1]) * (pAhatA[(m + k * M) + 1]) *
+           pxi[m + 1])
+      consist[(4 * k + 2) + 1] <- consist[(4 * k + 1) + 1]
+      consist[(4 * k + 3) + 1] <- consist[(4 * k + 3) + 1] +
+        ((pAhatA[(m + k * M) + 1]) * (pAhatA[(m + k * M) + 1]) * pxi[m + 1])
+    }
+  }
+  new_consist <- array(consist, c(2, 2, K),
+                       dimnames = list(c("ahat1.0", "ahat1.1"),
+                                       c("ahat2.0", "ahat2.1"),
+                                       att_names))
+
+  ### Final List -----
+  ret_list <- list(
+    posterior = class_probs %>%
+      as.matrix() %>%
+      unname(),
+    eap = attr_probs %>%
+      as.matrix() %>%
+      unname(),
+    acc = new_acc,
+    consist = new_consist,
+    gamma = gamma,
+    prev = prev,
+    pxi = pxi
+  )
+  return(ret_list)
+}
+
+reli_list <- function(model) {
+  N <- dplyr::n_distinct(model$data$data$resp_id)
+  K <- ncol(model$data$qmatrix) - 1L
   M  <- 2 ^ K
-  A <- c(0L, 1L)
+  A <- create_profiles(K) %>%
+    as.matrix() %>%
+    t() %>%
+    as.vector()
   pAhatA <- double(M * K)
 
-  attr_probs <- predict(model) %>%
-    dplyr::select(`1` = mean)
-  class_probs <- attr_probs %>%
-    dplyr::mutate(`0` = 1 - .data$`1`, .before = 1)
+  probs <- lapply(predict(model, summary = FALSE),
+                  clean_predicted_probs, resp_id = model$data$resp_id)
+  class_probs <- probs$class_probabilities
+  attr_probs <- probs$attribute_probabilities
 
   # map estimates
   ahat <- attr_probs %>%
@@ -33,99 +125,28 @@ create_reli_list_optim <- function(model) {
     as.vector()
 
   # structural parameters
-  if (model$backend == "cmdstanr") {
-    pxi <- tidybayes::spread_draws(model$model, (!!sym("nu"))[!!sym("c")]) %>%
-      dplyr::summarize(nu = mean(.data$nu)) %>%
-      dplyr::pull(.data$nu)
-  } else if (model$backend == "rstan") {
-    pxi <- model$model$par %>%
+  pxi <- if (model$backend == "rstan" && model$method == "optim") {
+    model$model$par %>%
       tibble::enframe() %>%
-      dplyr::filter(stringr::str_detect(.data$name, "^nu")) %>%
+      dplyr::filter(stringr::str_detect(.data$name, "^Vc")) %>%
       dplyr::pull(.data$value)
+  } else {
+    posterior::as_draws_df(model$model) %>%
+      tibble::as_tibble() %>%
+      dplyr::select(dplyr::matches("^Vc")) %>%
+      dplyr::summarize(dplyr::across(dplyr::everything(),
+                                     \(x) mean(x, na.rm = TRUE))) %>%
+      as.numeric()
   }
 
-  # make format
-  res <- calc_format(N = N, K = K, M = M, A = A, pAhatA = pAhatA,
-                     attr_probs = attr_probs, class_probs = class_probs,
-                     ahat = ahat, amean = amean, pAx = pAx, pxi = pxi)
+  att_names <- model$data$qmatrix %>%
+    dplyr::select(-"item_id") %>%
+    colnames()
 
-  # coerce to lcdmrfit
-  mod_dat <- model$data_lookup$data %>%
-    tidyr::pivot_wider(names_from = .data$item_id,
-                       values_from = .data$score) %>%
-    dplyr::select(-.data$resp_id) %>%
-    as.matrix()
+  res <- create_reli_list(N = N, K = K, M = M, A = A, pAhatA = pAhatA,
+                          attr_probs = attr_probs, class_probs = class_probs,
+                          ahat = ahat, amean = amean, pAx = pAx, pxi = pxi,
+                          att_names = att_names)
 
-  num_items <- nrow(model$data_lookup$item_lookup)
-  item_models <- list(~a1) %>%
-    rep(times = num_items) %>%
-    purrr::set_names(glue::glue("Item{seq_len(num_items)}"))
-
-  if (model$backend == "cmdstanr") {
-    item_params <- model$model %>%
-      tidybayes::spread_draws((!!sym("intercept"))[!!sym("i")],
-                              (!!sym("maineffect"))[!!sym("i")]) %>%
-      dplyr::summarize(`(Intercept)` = mean(.data$intercept),
-                       a1 = mean(.data$maineffect)) %>%
-      tidyr::nest(params = -.data$i) %>%
-      purrr::pmap(function(i, params) {
-        params %>%
-          tidyr::pivot_longer(dplyr::everything(), names_to = "param",
-                              values_to = "value") %>%
-          tibble::deframe()
-      }) %>%
-      purrr::set_names(glue::glue("Item{seq_len(num_items)}"))
-  } else if (model$backend == "rstan") {
-    item_params <- model$model$par %>%
-      tibble::enframe() %>%
-      dplyr::filter(stringr::str_detect(.data$name,
-                                        "^intercept|^maineffect")) %>%
-      tidyr::separate(.data$name, c("param", "i", NA), sep = "\\[|\\]") %>%
-      tidyr::pivot_wider(names_from = .data$param,
-                         values_from = .data$value) %>%
-      dplyr::rename(`(Intercept)` = .data$intercept, a1 = .data$maineffect) %>%
-      tidyr::nest(params = -.data$i) %>%
-      purrr::pmap(function(i, params) {
-        params %>%
-          tidyr::pivot_longer(dplyr::everything(), names_to = "param",
-                              values_to = "value") %>%
-          tibble::deframe()
-      }) %>%
-      purrr::set_names(glue::glue("Item{seq_len(num_items)}"))
-  }
-
-  mod_map <- res$eap %>%
-    tibble::as_tibble(.name_repair = ~"a1") %>%
-    dplyr::mutate(a1 = round(.data$a1, digits = 0),
-                  a1 = as.integer(.data$a1)) %>%
-    as.matrix() %>%
-    unname()
-
-  if (model$backend == "cmdstanr") {
-    log_lik <- model$model$lp()
-  } else if (model$backend == "rstan") {
-    log_lik <- model$model$par %>%
-      tibble::enframe() %>%
-      dplyr::filter(stringr::str_detect(.data$name, "^log_lik")) %>%
-      dplyr::pull(.data$value) %>%
-      sum()
-  }
-
-
-  lcdmr::lcdmrfit(x = mod_dat,
-                  item_models = item_models,
-                  beta = item_params,
-                  posterior = res$posterior,
-                  map = mod_map,
-                  eap = res$eap,
-                  log_lik = log_lik,
-                  acc = res$acc,
-                  consist = res$consist,
-                  gamma = res$gamma,
-                  prev = res$prev,
-                  pxi = res$pxi)
-}
-
-create_reli_list_mcmc <- function(x) {
-
+  return(res)
 }
