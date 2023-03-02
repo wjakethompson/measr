87d533be7963a4d49ce1cf26af00fa37bc348c52
Jake Thompson <wjakethompson@gmail.com>
2023-03-02 12:08:27 -0600

    add loo_compare method

--- R/loo-methods.R
+++ R/loo-methods.R
@@ -63,8 +63,55 @@ waic.measrfit <- function(x, ...) { #nolint
   loo::waic(log_lik_array, ...)
 }
 
+#' Relative model fit comparisons
+#'
+#' A [loo::loo_compare()] method that is customized for `measrfit` objects. See
+#' the **loo** package [vignettes](https://mc-stan.org/loo/articles/) for
+#' details.
+#'
+#' @param x A [measrfit] object.
+#' @param ... Additional objects of class [measrfit].
+#' @param criterion The name of the criterion to be extracted from the
+#'   [measrfit] object for comparison.
+#' @param model_names
+#'
+#' @return The object returned by [loo::loo_compare()].
+#'
 #' @export
-add_criterion <- function(x, criterion, overwrite = FALSE, force_save = FALSE,
-                          ...) {
+loo_compare.measrfit <- function(x, ..., criterion = c("loo", "waic"),
+                                 model_names = NULL) {
+  x <- check_model(x, required_class = "measrfit", name = "x")
+  ext_models <- list(...)
+  ext_models <- lapply(ext_models, check_model, required_class = "measrfit",
+                       name = "...", list = TRUE)
+  all_models <- c(list(x), ext_models)
 
+  criterion <- rlang::arg_match(criterion, values = c("loo", "waic"))
+  model_names <- vapply(model_names, check_character, character(1),
+                        allow_null = TRUE, name = "model_names",
+                        USE.NAMES = FALSE)
+
+  if (length(model_names) == 0) {
+    model_names <- vapply(all_models, \(x) x$type, character(1))
+  } else if (length(model_names) != length(all_models)) {
+    abort_bad_argument(arg = "model_names",
+                       must = glue::glue("be of length {length(all_models)}, ",
+                                         "the same as the number of models ",
+                                         "provided"),
+                       not = length(model_names))
+  }
+
+  loo_list <- lapply(all_models, \(x) x[["criteria"]][[criterion]])
+  loo_list <- rlang::set_names(loo_list, nm = model_names)
+  missing_crit <- vapply(loo_list, is.null, logical(1))
+
+  if (any(missing_crit)) {
+    rlang::abort("error_missing_criterion",
+                 message = glue::glue("Model {which(missing_crit)} does not ",
+                                      "contain a precomputed \"{criterion}\". ",
+                                      "See `?measr::add_criterion()` for ",
+                                      "help."))
+  }
+
+  loo::loo_compare(loo_list)
 }
