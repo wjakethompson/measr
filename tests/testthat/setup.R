ce37ddbab5e7b504b616bcdfb4527606e5a64c3e
Jake Thompson <wjakethompson@gmail.com>
2023-03-05 15:22:07 -0600

    don't set seeds

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,7 +3,7 @@ out <- capture.output(
     rstn_mdm_lcdm <- measr_dcm(
       data = mdm_data, missing = NA, qmatrix = mdm_qmatrix,
       resp_id = "respondent", item_id = "item", type = "lcdm",
-      method = "optim", seed = 63277, backend = "rstan",
+      method = "optim", backend = "rstan",
       prior = c(prior(uniform(-15, 15), class = "intercept"),
                 prior(uniform(0, 15), class = "maineffect"),
                 prior(uniform(-15, 15), class = "interaction")))
@@ -14,7 +14,7 @@ out <- capture.output(
   suppressMessages(
     rstn_dina <- measr_dcm(data = dina_data, missing = NA, qmatrix = q_matrix,
                            resp_id = "resp_id", item_id = "item", type = "dina",
-                           method = "optim", seed = 63277, backend = "rstan")
+                           method = "optim", backend = "rstan")
   )
 )
 
@@ -22,6 +22,6 @@ out <- capture.output(
   suppressMessages(
     rstn_dino <- measr_dcm(data = dino_data, missing = NA, qmatrix = q_matrix,
                            resp_id = "resp_id", item_id = "item", type = "dino",
-                           method = "optim", seed = 63277, backend = "rstan")
+                           method = "optim", backend = "rstan")
   )
 )
