3b4b163377bd5663e4e055cf9759235f273d59ac
Jake Thompson <wjakethompson@gmail.com>
2023-03-21 11:35:12 -0500

    try even smaller examples

--- R/ppmc.R
+++ R/ppmc.R
@@ -95,7 +95,7 @@
 #'   data = mdm_data, missing = NA, qmatrix = mdm_qmatrix,
 #'   resp_id = "respondent", item_id = "item", type = "dina",
 #'   method = "mcmc", seed = 63277, backend = "rstan",
-#'   iter = 700, warmup = 500, chains = 1,
+#'   iter = 350, warmup = 250, chains = 1,
 #'   prior = c(prior(beta(5, 17), class = "slip"),
 #'             prior(beta(5, 17), class = "guess"))
 #' )
