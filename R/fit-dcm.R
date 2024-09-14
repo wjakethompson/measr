04d14795b1ecf6bced504d4c6ee3042c0ffdce34
dgkf <18220321+dgkf@users.noreply.github.com>
2024-09-14 18:08:47 -0400

    fix: adding back in R in R expressions

--- R/fit-dcm.R
+++ R/fit-dcm.R
@@ -6,8 +6,8 @@
 #'
 #' @param data Response data. A data frame with 1 row per respondent and 1
 #'   column per item.
-#' @param missing An expression specifying how missing data in `data` is coded
-#'   (e.g., `NA`, `"."`, `-99`, etc.). The default is `NA`.
+#' @param missing An `R` expression specifying how missing data in `data` is
+#'   coded (e.g., `NA`, `"."`, `-99`, etc.). The default is `NA`.
 #' @param qmatrix The Q-matrix. A data frame with 1 row per item and 1 column
 #'   per attribute. All cells should be either 0 (item does not measure the
 #'   attribute) or 1 (item does measure the attribute).
