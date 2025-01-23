15917600c34af82613cefc0130e49ac03006b36f
Jake Thompson <wjakethompson@gmail.com>
2025-01-23 09:46:47 -0600

    remove stringr dependency

--- R/utils-model-evaluation.R
+++ R/utils-model-evaluation.R
@@ -69,8 +69,8 @@ aic <- function(model) {
   num_params <- model$model$par %>%
     tibble::as_tibble() %>%
     dplyr::mutate(param = names(model$model$par)) %>%
-    dplyr::filter(!stringr::str_detect(.data$param, "pi")) %>%
-    dplyr::filter(!stringr::str_detect(.data$param, "log_Vc")) %>%
+    dplyr::filter(!grepl("pi", .data$param),
+                  !grepl("log_Vc", .data$param)) %>%
     nrow() - 1
 
   aic <- (-2 * logLik) + (2 * num_params)
@@ -84,8 +84,8 @@ bic <- function(model) {
   num_params <- model$model$par %>%
     tibble::as_tibble() %>%
     dplyr::mutate(param = names(model$model$par)) %>%
-    dplyr::filter(!stringr::str_detect(.data$param, "pi")) %>%
-    dplyr::filter(!stringr::str_detect(.data$param, "log_Vc")) %>%
+    dplyr::filter(!grepl("pi", .data$param),
+                  !grepl("log_Vc", .data$param)) %>%
     nrow() - 1
 
   N <- model$data$data %>%
