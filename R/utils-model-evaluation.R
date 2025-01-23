c71fdb0d38ac33944efb6ac60f467d89092d346e
Jake Thompson <wjakethompson@gmail.com>
2025-01-23 09:57:52 -0600

    move aic/bic to own script

--- R/utils-model-evaluation.R
+++ R/utils-model-evaluation.R
@@ -62,37 +62,3 @@ add_ppmc <- function(model, run_ppmc) {
 
   return(model)
 }
-
-aic <- function(model) {
-  logLik <- model$model$value
-
-  num_params <- model$model$par %>%
-    tibble::as_tibble() %>%
-    dplyr::mutate(param = names(model$model$par)) %>%
-    dplyr::filter(!grepl("pi", .data$param),
-                  !grepl("log_Vc", .data$param)) %>%
-    nrow() - 1
-
-  aic <- (-2 * logLik) + (2 * num_params)
-
-  return(aic)
-}
-
-bic <- function(model) {
-  logLik <- model$model$value
-
-  num_params <- model$model$par %>%
-    tibble::as_tibble() %>%
-    dplyr::mutate(param = names(model$model$par)) %>%
-    dplyr::filter(!grepl("pi", .data$param),
-                  !grepl("log_Vc", .data$param)) %>%
-    nrow() - 1
-
-  N <- model$data$data %>%
-    dplyr::distinct(.data$resp_id) %>%
-    nrow()
-
-  bic <- (-2 * logLik) + (log(N) * num_params)
-
-  return(bic)
-}
