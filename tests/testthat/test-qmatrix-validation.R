711727efafbe61252a70353bc4787e9c1a650abc
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-04-23 12:46:33 -0400

    estimate optim model for unit test

--- tests/testthat/test-qmatrix-validation.R
+++ tests/testthat/test-qmatrix-validation.R
@@ -1,4 +1,11 @@
 test_that("Q-matrix validation works for ecpe", {
+  cmds_ecpe_lcdm <- measr_dcm(
+    data = ecpe_data, missing = NA, qmatrix = ecpe_qmatrix,
+    resp_id = "resp_id", item_id = "item_id", type = "lcdm",
+    method = "optim", seed = 63277, backend = "cmdstanr",
+    prior = c(prior(uniform(-15, 15), class = "intercept"),
+              prior(uniform(0, 15), class = "maineffect"),
+              prior(uniform(-15, 15), class = "interaction")))
   cmds_ecpe_lcdm <- add_respondent_estimates(cmds_ecpe_lcdm)
   tmp_mod <- add_qmatrix_validation(mod = cmds_ecpe_lcdm)
 
