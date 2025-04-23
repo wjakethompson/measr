ed32208b0efd156d836ac09fe1d505384798df69
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-04-23 15:39:58 -0400

    suppress output

--- tests/testthat/test-qmatrix-validation.R
+++ tests/testthat/test-qmatrix-validation.R
@@ -1,11 +1,15 @@
 test_that("Q-matrix validation works for ecpe", {
-  cmds_ecpe_lcdm <- measr_dcm(
-    data = ecpe_data, missing = NA, qmatrix = ecpe_qmatrix,
-    resp_id = "resp_id", item_id = "item_id", type = "lcdm",
-    method = "optim", seed = 63277, backend = "cmdstanr",
-    prior = c(prior(uniform(-15, 15), class = "intercept"),
-              prior(uniform(0, 15), class = "maineffect"),
-              prior(uniform(-15, 15), class = "interaction")))
+  out <- capture.output(
+    suppressMessages(
+      cmds_ecpe_lcdm <- measr_dcm(
+        data = ecpe_data, missing = NA, qmatrix = ecpe_qmatrix,
+        resp_id = "resp_id", item_id = "item_id", type = "lcdm",
+        method = "optim", seed = 63277, backend = "cmdstanr",
+        prior = c(prior(uniform(-15, 15), class = "intercept"),
+                  prior(uniform(0, 15), class = "maineffect"),
+                  prior(uniform(-15, 15), class = "interaction")))
+    )
+  )
   cmds_ecpe_lcdm <- add_respondent_estimates(cmds_ecpe_lcdm)
   tmp_mod <- add_qmatrix_validation(mod = cmds_ecpe_lcdm)
 
@@ -14,6 +18,6 @@ test_that("Q-matrix validation works for ecpe", {
                  "empirical_specification", "pvaf"))
   expect_equal(nrow(tmp_mod$qmatrix_validation), 28)
   expect_equal(nrow(tmp_mod$qmatrix_validation %>%
-                      filter(!validation_flag)),
+                      dplyr::filter(!validation_flag)),
                28)
 })
