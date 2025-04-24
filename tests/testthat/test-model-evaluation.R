3b8bd1a1ff3f91bc08ddfdf4c602aa0662c01569
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-04-24 10:34:05 -0400

    elaborate on unit tests for add_bayes_factor()

--- tests/testthat/test-model-evaluation.R
+++ tests/testthat/test-model-evaluation.R
@@ -38,3 +38,61 @@ test_that("BIC works", {
 
   expect_equal(rstn_dino$criteria$bic, exp_bic, tolerance = .0001)
 })
+
+if (!identical(Sys.getenv("NOT_CRAN"), "true")) {
+  skip("No MCMC on CRAN")
+} else {
+  out <- capture.output(
+    suppressMessages(
+      cmds_mdm_lcdm <- measr_dcm(
+        data = mdm_data, missing = NA, qmatrix = mdm_qmatrix,
+        resp_id = "respondent", item_id = "item", type = "lcdm",
+        method = "mcmc", seed = 63277, backend = "cmdstanr",
+        iter_sampling = 500, iter_warmup = 1000, chains = 2,
+        parallel_chains = 2,
+        prior = c(prior(uniform(-15, 15), class = "intercept"),
+                  prior(uniform(0, 15), class = "maineffect")))
+    )
+  )
+
+  out <- capture.output(
+    suppressMessages(
+      cmds_mdm_dina <- measr_dcm(
+        data = mdm_data, missing = NA, qmatrix = mdm_qmatrix,
+        resp_id = "respondent", item_id = "item", type = "dina",
+        attribute_structure = "independent",
+        method = "mcmc", seed = 63277, backend = "rstan",
+        iter = 1500, warmup = 1000, chains = 2,
+        cores = 2, refresh = 0,
+        prior = c(prior(beta(5, 17), class = "slip"),
+                  prior(beta(5, 17), class = "guess")))
+    )
+  )
+}
+
+test_that("Bayes factor works", {
+  tmp_mod <- add_bayes_factor(cmds_mdm_lcdm, cmds_mdm_dina)
+
+  expect_equal(length(tmp_mod$bayes_factor), 3)
+  expect_equal(names(tmp_mod$bayes_factor),
+               c("bf", "comp_model", "posterior_probability"))
+  expect_equal(typeof(tmp_mod$bayes_factor$bf), "double")
+  expect_equal(tmp_mod$bayes_factor$comp_model, "dina")
+  expect_equal(class(tmp_mod$bayes_factor$posterior_probability),
+               c("tbl_df", "tbl", "data.frame"))
+  expect_equal(ncol(tmp_mod$bayes_factor$posterior_probability), 10)
+  expect_equal(nrow(tmp_mod$bayes_factor$posterior_probability), 2)
+  expect_equal(tmp_mod$bayes_factor$posterior_probability$model[1],
+               cmds_mdm_lcdm$type)
+  expect_equal(tmp_mod$bayes_factor$posterior_probability$model[2],
+               cmds_mdm_lcdm$cmds_mdm_dina)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,2]), 1)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,3]), 1)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,4]), 1)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,5]), 1)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,6]), 1)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,7]), 1)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,8]), 1)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,9]), 1)
+  expect_equal(sum(tmp_mod$bayes_factor$posterior_probability[,10]), 1)
+})
