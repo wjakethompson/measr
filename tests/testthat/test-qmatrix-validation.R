0f3db9255cb77fd32ce96850122c0255f41a1dd4
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-05-20 11:21:10 -0400

    update for refactor
    Merge branch 'tmp3' into qmatrix-validation

--- tests/testthat/test-qmatrix-validation.R
+++ tests/testthat/test-qmatrix-validation.R
@@ -1,23 +1,23 @@
 test_that("Q-matrix validation works for ecpe", {
+  mod_spec <- dcm_specify(qmatrix = dcmdata::ecpe_qmatrix,
+                          identifier = "item_id")
+
   out <- capture.output(
     suppressMessages(
-      cmds_ecpe_lcdm <- measr_dcm(
-        data = ecpe_data, missing = NA, qmatrix = ecpe_qmatrix,
-        resp_id = "resp_id", item_id = "item_id", type = "lcdm",
-        method = "optim", seed = 63277, backend = "cmdstanr",
-        prior = c(prior(uniform(-15, 15), class = "intercept"),
-                  prior(uniform(0, 15), class = "maineffect"),
-                  prior(uniform(-15, 15), class = "interaction")))
+      rstn_ecpe_lcdm <- dcm_estimate(mod_spec, data = dcmdata::ecpe_data,
+                                     identifier = "resp_id",
+                                     backend = "rstan",
+                                     method = "optim")
     )
   )
-  cmds_ecpe_lcdm <- add_respondent_estimates(cmds_ecpe_lcdm)
-  tmp_mod <- add_qmatrix_validation(mod = cmds_ecpe_lcdm)
+  rstn_ecpe_lcdm <- add_respondent_estimates(rstn_ecpe_lcdm)
+  tmp_mod <- add_qmatrix_validation(mod = rstn_ecpe_lcdm)
 
-  expect_equal(names(tmp_mod$qmatrix_validation),
+  expect_equal(names(tmp_mod@qmatrix_validation),
                c("item_id", "validation_flag", "original_specification",
                  "empirical_specification", "pvaf"))
-  expect_equal(nrow(tmp_mod$qmatrix_validation), 28)
-  expect_equal(nrow(tmp_mod$qmatrix_validation %>%
+  expect_equal(nrow(tmp_mod@qmatrix_validation), 28)
+  expect_equal(nrow(tmp_mod@qmatrix_validation %>%
                       dplyr::filter(!validation_flag)),
                28)
 })
