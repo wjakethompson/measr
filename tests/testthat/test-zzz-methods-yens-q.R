88dcbdd7f6a4b564e34b27926637897b211891d0
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-05-30 16:02:49 -0400

    add unit test for Yen's Q calc

---
+++ tests/testthat/test-zzz-methods-yens-q.R
@@ -0,0 +1,9 @@
+test_that("Yen's Q works", {
+  mod <- add_respondent_estimates(rstn_dina)
+  yens_output <- yens_q(mod)
+
+  expect_equal(nrow(yens_output), 595)
+  expect_equal(ncol(yens_output), 4)
+  expect_equal(names(yens_output), c("item_id", "item_id_2", "resid_corr",
+                                     "flag"))
+})
