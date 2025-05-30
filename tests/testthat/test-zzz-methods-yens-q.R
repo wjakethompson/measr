f65477022a755fbc8b7dee9512119e968cfe4715
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-05-30 16:04:05 -0400

    add unit test for Yen's Q with dino

--- tests/testthat/test-zzz-methods-yens-q.R
+++ tests/testthat/test-zzz-methods-yens-q.R
@@ -6,4 +6,12 @@ test_that("Yen's Q works", {
   expect_equal(ncol(yens_output), 4)
   expect_equal(names(yens_output), c("item_id", "item_id_2", "resid_corr",
                                      "flag"))
+
+  mod2 <- add_respondent_estimates(rstn_dino)
+  yens_output <- yens_q(mod2)
+
+  expect_equal(nrow(yens_output), 595)
+  expect_equal(ncol(yens_output), 4)
+  expect_equal(names(yens_output), c("item_id", "item_id_2", "resid_corr",
+                                     "flag"))
 })
