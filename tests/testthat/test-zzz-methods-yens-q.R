75f23aa3243c19f27b3b126f46cb70e8cd778a37
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-06-02 11:26:24 -0400

    fine-tuning unit tests

--- tests/testthat/test-zzz-methods-yens-q.R
+++ tests/testthat/test-zzz-methods-yens-q.R
@@ -8,10 +8,10 @@ test_that("Yen's Q works", {
                                      "flag"))
 
   mod2 <- add_respondent_estimates(rstn_dino)
-  yens_output <- yens_q(mod2)
+  yens_output2 <- yens_q(mod2)
 
-  expect_equal(nrow(yens_output), 595)
-  expect_equal(ncol(yens_output), 4)
-  expect_equal(names(yens_output), c("item_id", "item_id_2", "resid_corr",
+  expect_equal(nrow(yens_output2), 595)
+  expect_equal(ncol(yens_output2), 4)
+  expect_equal(names(yens_output2), c("item_id", "item_id_2", "resid_corr",
                                      "flag"))
 })
