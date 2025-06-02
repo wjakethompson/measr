3fd5b713499e5fcd1cd2dfd00e2b977d4d79dc33
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-06-02 14:50:44 -0400

    update bf unit tests

--- tests/testthat/test-zzz-methods-bayes-factor.R
+++ tests/testthat/test-zzz-methods-bayes-factor.R
@@ -25,38 +25,41 @@ if (!identical(Sys.getenv("NOT_CRAN"), "true")) {
                                     backend = "rstan", method = "mcmc")
     )
   )
+
+  rstn_mdm_lcdm <- add_respondent_estimates(rstn_mdm_lcdm)
+  rstn_mdm_dina <- add_respondent_estimates(rstn_mdm_dina)
 }
 
 test_that("Bayes factor works", {
-  tmp_mod <- add_bayes_factor(rstn_mdm_lcdm, rstn_mdm_dina)
+  bf_res <- bayes_factor(rstn_mdm_lcdm, rstn_mdm_dina)
 
-  expect_equal(length(tmp_mod@bayes_factor), 3)
-  expect_equal(names(tmp_mod@bayes_factor),
+  expect_equal(length(bf_res), 3)
+  expect_equal(names(bf_res),
                c("bf", "comp_model", "posterior_probability"))
-  expect_equal(typeof(tmp_mod@bayes_factor$bf), "double")
-  expect_equal(tmp_mod@bayes_factor$comp_model, "dina")
-  expect_equal(class(tmp_mod@bayes_factor$posterior_probability),
+  expect_equal(typeof(bf_res$bf), "double")
+  expect_equal(bf_res$comp_model, "dina")
+  expect_equal(class(bf_res$posterior_probability),
                c("tbl_df", "tbl", "data.frame"))
-  expect_equal(ncol(tmp_mod@bayes_factor$posterior_probability), 10)
-  expect_equal(nrow(tmp_mod@bayes_factor$posterior_probability), 2)
-  expect_equal(tmp_mod@bayes_factor$posterior_probability$model[1],
+  expect_equal(ncol(bf_res$posterior_probability), 10)
+  expect_equal(nrow(bf_res$posterior_probability), 2)
+  expect_equal(bf_res$posterior_probability$model[1],
                rstn_mdm_lcdm@model_spec@measurement_model@model)
-  expect_equal(tmp_mod@bayes_factor$posterior_probability$model[2],
+  expect_equal(bf_res$posterior_probability$model[2],
                rstn_mdm_dina@model_spec@measurement_model@model)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 2]), 1)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 3]), 1)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 4]), 1)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 5]), 1)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 6]), 1)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 7]), 1)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 8]), 1)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 9]), 1)
-  expect_equal(sum(tmp_mod@bayes_factor$posterior_probability[, 10]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 2]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 3]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 4]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 5]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 6]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 7]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 8]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 9]), 1)
+  expect_equal(sum(bf_res$posterior_probability[, 10]), 1)
 })
 
 test_that("Add marginal likelihood works", {
-  tmp_mod2 <- add_marginal_likelihood(rstn_mdm_lcdm)
+  mll_res <- add_marginal_likelihood(rstn_mdm_lcdm)
 
-  expect_equal(typeof(tmp_mod2@log_marginal_likelihood$logml), "double")
-  expect_equal(length(tmp_mod2@log_marginal_likelihood$logml), 1)
+  expect_equal(typeof(mll_res), "double")
+  expect_equal(length(mll_res), 1)
 })
