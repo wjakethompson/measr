d41aaddbb979b34d5069902c1147fe334ef91f5a
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-08-14 14:00:26 -0400

    use existing model; test single-attribute model

--- tests/testthat/test-zzz-methods-qmatrix-validation.R
+++ tests/testthat/test-zzz-methods-qmatrix-validation.R
@@ -12,18 +12,34 @@ test_that("Q-matrix validation works for ecpe", {
       "pvaf"
     )
   )
-  expect_equal(nrow(qmat_valid_res), 28)
+  expect_equal(nrow(qmat_valid_res), 35)
   expect_equal(
     nrow(
       qmat_valid_res |>
         dplyr::filter(!validation_flag)
     ),
-    28
+    35
+  )
+
+  dina_spec <- dcmstan::dcm_specify(
+    qmatrix = dcmdata::mdm_qmatrix,
+    identifier = "item",
+    measurement_model = dina()
+  )
+  suppressMessages(
+    dina_mod <- dcm_estimate(
+      dina_spec,
+      data = dcmdata::mdm_data,
+      identifier = "respondent",
+      method = "optim",
+      backend = "rstan",
+      seed = 63277
+    )
   )
 
   err <- rlang::catch_cnd(qmatrix_validation(x = dina_mod))
   expect_match(
     err$message,
-    "The Q-matrix validation method can\\nonly be applied to assessments"
+    "The Q-matrix validation method can only be applied to assessments measuring more than one attribute\."
   )
 })
