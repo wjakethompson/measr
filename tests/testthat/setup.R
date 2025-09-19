4f028e0b22cf6c9f0b37220896be27c34e68579c
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-09-19 10:46:54 -0400

    add lcdm to setup for q-matrix validation testing

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -17,6 +17,12 @@ dino_spec <- dcmstan::dcm_specify(
   measurement_model = dino()
 )
 
+lcdm_spec <- dcmstan::dcm_specify(
+  qmatrix = dcmdata::ecpe_qmatrix |>
+    dplyr::select(-"item_id"),
+  measurement_model = lcdm()
+)
+
 out <- capture.output(
   suppressMessages(
     rstn_dina <- dcm_estimate(
@@ -40,7 +46,20 @@ out <- capture.output(
       method = "optim",
       backend = "rstan",
       seed = 63277,
-      precompiled = stanmodels$test_dina
+      precompiled = measr:::stanmodels$test_dina
+    )
+  )
+)
+
+out <- capture.output(
+  suppressMessages(
+    rstn_lcdm <- dcm_estimate(
+      lcdm_spec,
+      data = dcmdata::ecpe_data,
+      identifier = "resp_id",
+      method = "optim",
+      backend = "rstan",
+      seed = 63277
     )
   )
 )
