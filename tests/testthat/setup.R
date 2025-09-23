337f69968416a3725b1ff6d5a331247ef150c3aa
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2025-09-23 11:06:48 -0400

    no longer misspecify DINO

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -6,14 +6,7 @@ dina_spec <- dcmstan::dcm_specify(
 # estimate dino with misspecified Q-matrix for items 1 & 3 (over) and
 # items 5 & 9 (under)
 dino_spec <- dcmstan::dcm_specify(
-  qmatrix = dplyr::mutate(
-    q_matrix,
-    att1 = dplyr::case_when(.data$item == "item_1" ~ 1L, .default = .data$att1),
-    att2 = dplyr::case_when(.data$item == "item_3" ~ 1L, .default = .data$att2),
-    att4 = dplyr::case_when(.data$item == "item_9" ~ 0L, .default = .data$att4),
-    att5 = dplyr::case_when(.data$item == "item_5" ~ 0L, .default = .data$att5),
-  ),
-  identifier = "item",
+  qmatrix = q_matrix[, -1],
   measurement_model = dino()
 )
 
